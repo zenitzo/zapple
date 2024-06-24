7f18c7900a65e9cd76398bcbe962c8fc3052f1a9
zenitzo <christiang.dev@gmail.com>
2024-06-24 19:45:08 -0400

    main

--- src/utils/animations.js
+++ src/utils/animations.js
@@ -1,6 +1,6 @@
-import gsap from "gsap"
+import gsap from 'gsap';
 
-import { ScrollTrigger } from "gsap/all"
+import { ScrollTrigger } from 'gsap/all';
 gsap.registerPlugin(ScrollTrigger);
 
 export const animateWithGsap = (target, animationProps, scrollProps) => {
@@ -11,32 +11,39 @@ export const animateWithGsap = (target, animationProps, scrollProps) => {
       toggleActions: 'restart reverse restart reverse',
       start: 'top 85%',
       ...scrollProps,
-    }
-  })
-}
+    },
+  });
+};
 
-export const animateWithGsapTimeline = (timeline, rotationRef, rotationState, firstTarget, secondTarget, animationProps) => {
+export const animateWithGsapTimeline = (
+  timeline,
+  rotationRef,
+  rotationState,
+  firstTarget,
+  secondTarget,
+  animationProps
+) => {
   timeline.to(rotationRef.current.rotation, {
     y: rotationState,
     duration: 1,
-    ease: 'power2.inOut'
-  })
+    ease: 'power2.inOut',
+  });
 
   timeline.to(
     firstTarget,
     {
       ...animationProps,
-      ease: 'power2.inOut'
+      ease: 'power2.inOut',
     },
     '<'
-  )
+  );
 
   timeline.to(
     secondTarget,
     {
       ...animationProps,
-      ease: 'power2.inOut'
+      ease: 'power2.inOut',
     },
     '<'
-  )
-}
+  );
+};
