aa37eff6e27d30c1e39019f504878271a28f9aac
zenitzo <christiang.dev@gmail.com>
2024-06-24 20:05:25 -0400

    I WILL FIGURE THIS OUT

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -30,7 +30,7 @@ const Hero = () => {
   }, []);
 
   return (
-    <section className="w-full nav-height bg-black relative">
+    <section className="w-full nav-height bg-black relative overflow-scroll scroll-smooth">
       <div className="h-5/6 w-full flex-center flex-col">
         <p id="hero" className="hero-title">
           iPhone 15 Pro
